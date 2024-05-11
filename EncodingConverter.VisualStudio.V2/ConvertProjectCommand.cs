a453b1de43078c97be5f6485a7611dbec62f7b6a
Jonghyo Lee <na1307@outlook.kr>
2024-05-11 21:01:28 +0900

    V2 Base

---
+++ EncodingConverter.VisualStudio.V2/ConvertProjectCommand.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.Extensibility;
+using Microsoft.VisualStudio.Extensibility.Commands;
+
+namespace EncodingConverter.VisualStudio;
+
+/// <summary>
+/// ConvertProjectCommand handler.
+/// </summary>
+[VisualStudioContribution]
+internal sealed class ConvertProjectCommand : Command {
+    [VisualStudioContribution]
+    public static CommandGroupConfiguration ConvertProjectGroup => new(GroupPlacement.VsctParent(Guid.Parse("{d309f791-903f-11d0-9efc-00a0c911004f}"), 0x0402, 0x0600)) {
+        Children = [GroupChild.Command<ConvertProjectCommand>()]
+    };
+
+    /// <inheritdoc />
+    public override CommandConfiguration CommandConfiguration => new("%EncodingConverter.VisualStudio.ConvertProjectCommand.DisplayName%") {
+        // Use this object initializer to set optional parameters for the command. The required parameter,
+        // displayName, is set above. DisplayName is localized and references an entry in .vsextension\string-resources.json.
+        Icon = new(ImageMoniker.KnownValues.FileEncodingDialog, IconSettings.IconAndText),
+    };
+
+    /// <inheritdoc />
+    public override async Task ExecuteCommandAsync(IClientContext context, CancellationToken cancellationToken) {
+
+    }
+}
---
+++ EncodingConverter.VisualStudio.V2/ExtensionEntrypoint.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.Extensibility;
+
+namespace EncodingConverter.VisualStudio;
+
+/// <summary>
+/// Extension entrypoint for the VisualStudio.Extensibility extension.
+/// </summary>
+[VisualStudioContribution]
+internal sealed class ExtensionEntrypoint : Extension {
+    /// <inheritdoc />
+    public override ExtensionConfiguration ExtensionConfiguration => new() {
+        RequiresInProcessHosting = true,
+    };
+}
